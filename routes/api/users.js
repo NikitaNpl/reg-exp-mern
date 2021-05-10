deb41a1cc064127849eb1e928f32d95015a41f5b
NikitaNpl <totmyanin.n@bk.ru>
2021-05-10 17:11:55 +0300

    [edit] - произвел изменения для heroku

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,7 +31,7 @@ router.get('/oauth-callback/:code?', (req, res) => {
     }
   }).then((response) => {
     const accessToken = response.data.access_token;
-    return res.redirect(`./index.html/github-auth?access_token=${accessToken}`);
+    return res.redirect(`/github-auth?access_token=${accessToken}`);
   })
     .then((accessToken) => {
       return axios({
--- server.js
+++ server.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // Use Routes
 app.use('/api/items', items);
 app.use('/api/categories', categories);
